b6fab4ede665862091e23fe23018eeae23c100ec
ALEX-MUTHOMI <alexmuthomi64@outlook.com>
2026-01-16 10:38:21 +0300

    Added Api Documentation

--- app/core/models.py
+++ app/core/models.py
@@ -1,26 +1,26 @@
 """
-Database models for the application
+Database models.
 """
 from django.db import models
-
 from django.contrib.auth.models import (
     AbstractBaseUser,
     BaseUserManager,
-    PermissionsMixin
+    PermissionsMixin,
 )
 
-class userManager(BaseUserManager):
-    """Manager for users"""
+
+class UserManager(BaseUserManager):
+    """Manager for users."""
 
     def create_user(self, email, password=None, **extra_fields):
-      """Create and save a user with the given email and password"""
-      if not email:
-            raise ValueError("Users must have an email address")
-      user = self.model(email=self.normalize_email(email), **extra_fields) # <--- GOOD
-      user.set_password(password)
-      user.save(using=self.db)
+        """Create, save and return a new user."""
+        if not email:
+            raise ValueError('User must have an email address.')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
 
-      return user
+        return user
 
     def create_superuser(self, email, password):
         """Create and return a new superuser."""
@@ -32,14 +32,13 @@ class userManager(BaseUserManager):
         return user
 
 
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the system."""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
 
-class user(AbstractBaseUser, PermissionsMixin):
-       """This is the user in the system"""
-       email = models.EmailField(max_length=255, unique=True)
-       name = models.CharField(max_length=255)
-       is_active = models.BooleanField(default=True)
-       is_staff = models.BooleanField(default=False)
+    objects = UserManager()
 
-       objects = userManager()
-
-       USERNAME_FIELD = "email"
+    USERNAME_FIELD = 'email'
