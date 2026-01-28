8bcdb5c5cb4b594dc891a4397fc448a01b94a838
ALEX-MUTHOMI <alexmuthomi64@outlook.com>
2026-01-28 16:50:37 +0300

    Feature: Enable Recipe creation. Includes automated user ownership assignment and full integration tests.

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -12,10 +12,12 @@ class RecipeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Recipe
         fields = ['id', 'title', 'time_minutes', 'price', 'link']  # <--- [ ]
-        read_only_fields = ("id",)
+        read_only_fields = ["id",]
 
 
 class RecipeDetailSerializer(RecipeSerializer):
     """serializer for recipe detail view"""
     class Meta(RecipeSerializer.Meta):
         fields = RecipeSerializer.Meta.fields + ["description"]
+
+
