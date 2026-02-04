74118ed595b17aaf8e8c1edbe25f4be8e612a540
ALEX-MUTHOMI <alexmuthomi64@outlook.com>
2026-02-04 12:32:10 +0300

    Implemented Deleting TAGS with tests

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -88,3 +88,17 @@ class PrivateTagsApiTests(TestCase):
         # Verify the change happened in the DB
         tag.refresh_from_db()
         self.assertEqual(tag.name, payload['name'])
+
+    def test_delete_tag(self):
+        """Test deleting a TAG is successful"""
+        tag = Tag.objects.create(user=self.user, name = "Banana Bread")
+        
+        url = tag_detail_url(tag.id)
+        res = self.client.delete(url)
+
+        self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
+        tags = Tag.objects.filter(user=self.user)
+        self.assertFalse(tags.exists())
+
+
+
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -36,14 +36,17 @@ class RecipeViewSet(viewsets.ModelViewSet):
 
         return self.serializer_class
 
-class TagViewSet(mixins.UpdateModelMixin,
+class TagViewSet(mixins.DestroyModelMixin,
+                 mixins.UpdateModelMixin,
                  mixins.ListModelMixin,
                  viewsets.GenericViewSet):
     """Manage Tags In the Database"""
+    
     serializer_class = serializers.TagSerializer
     queryset = Tag.objects.all()
     authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]  
+    permission_classes = [IsAuthenticated]
+
     def get_queryset(self):
         """Filter queryset to authenticated user"""
         return self.queryset.filter(user=self.request.user).order_by('-name')
